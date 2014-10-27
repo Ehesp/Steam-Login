c1537f127e3575555d10540574732a1a0231fefe
Arnaud Ligny <arnaud@ligny.org>
2014-10-27 23:50:52 +0100

    Update SteamLoginServiceProvider.php

--- src/Ehesp/SteamLogin/Laravel/SteamLoginServiceProvider.php
+++ src/Ehesp/SteamLogin/Laravel/SteamLoginServiceProvider.php
@@ -6,25 +6,25 @@ use Ehesp\SteamLogin\SteamLogin;
 
 class SteamLoginServiceProvider extends ServiceProvider
 {
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app->bindShared('steamlogin', function($app) {
-			return new SteamLogin();
-		});
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bindShared('steamlogin', function($app) {
+            return new SteamLogin();
+        });
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('steamlogin');
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('steamlogin');
+    }
 }
