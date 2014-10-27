556c2ba8fdcf46b13ab205c630e627aa4fbf056c
Arnaud Ligny <arnaud@ligny.org>
2014-10-27 23:52:42 +0100

    Update SteamLogin.php

--- src/Ehesp/SteamLogin/SteamLogin.php
+++ src/Ehesp/SteamLogin/SteamLogin.php
@@ -10,7 +10,7 @@ class SteamLogin implements SteamLoginInterface
      *
      * @var string
      */
-	private static $openId = 'https://steamcommunity.com/openid/login';
+    private static $openId = 'https://steamcommunity.com/openid/login';
 
     /**
      * Validates a given URL, ensuring it contains the http or https URI Scheme
@@ -18,13 +18,13 @@ class SteamLogin implements SteamLoginInterface
      * @param string $url
      * @return bool
      */
-	private function validateUrl($url)
-	{
-		if (!filter_var($url, FILTER_VALIDATE_URL)) {
-	        return false;
-		}
-		return true;
-	}
+    private function validateUrl($url)
+    {
+        if (!filter_var($url, FILTER_VALIDATE_URL)) {
+            return false;
+        }
+        return true;
+    }
 
     /**
      * Build the Steam login URL
@@ -32,84 +32,84 @@ class SteamLogin implements SteamLoginInterface
      * @param string $return A custom return to URL
      * @return string
      */
-	public function url($return = null)
-	{
-		if (!is_null($return)) {
-			if (!$this->validateUrl($return)) {
+    public function url($return = null)
+    {
+        if (!is_null($return)) {
+            if (!$this->validateUrl($return)) {
                 throw new Exception('The return URL must be a valid URL with a URI Scheme or http or https.');
-			}
-		}
-		else {
-			$return = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'];
-		}
+            }
+        }
+        else {
+            $return = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'];
+        }
 
-		$params = array(
-			'openid.ns'         => 'http://specs.openid.net/auth/2.0',
-			'openid.mode'       => 'checkid_setup',
-			'openid.return_to'  => $return,
-			'openid.realm'      => (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'],
-			'openid.identity'   => 'http://specs.openid.net/auth/2.0/identifier_select',
-			'openid.claimed_id' => 'http://specs.openid.net/auth/2.0/identifier_select',
-		);
+        $params = array(
+            'openid.ns'         => 'http://specs.openid.net/auth/2.0',
+            'openid.mode'       => 'checkid_setup',
+            'openid.return_to'  => $return,
+            'openid.realm'      => (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'],
+            'openid.identity'   => 'http://specs.openid.net/auth/2.0/identifier_select',
+            'openid.claimed_id' => 'http://specs.openid.net/auth/2.0/identifier_select',
+        );
 
-		return self::$openId . '?' . http_build_query($params, '', '&amp;');
-	}
+        return self::$openId . '?' . http_build_query($params, '', '&amp;');
+    }
 
     /**
      * Validates a Steam login request and returns the users Steam Community ID
      *
      * @return string
      */
-	public function validate()
+    public function validate()
     {
-    	$response = null;
+        $response = null;
 
-    	try {
-    		$params = array(
-    			'openid.assoc_handle' => $_GET['openid_assoc_handle'],
-    			'openid.signed'       => $_GET['openid_signed'],
-    			'openid.sig'          => $_GET['openid_sig'],
-    			'openid.ns'           => 'http://specs.openid.net/auth/2.0',
+        try {
+            $params = array(
+                'openid.assoc_handle' => $_GET['openid_assoc_handle'],
+                'openid.signed'       => $_GET['openid_signed'],
+                'openid.sig'          => $_GET['openid_sig'],
+                'openid.ns'           => 'http://specs.openid.net/auth/2.0',
             );
 
-    		$signed = explode(',', $_GET['openid_signed']);
+            $signed = explode(',', $_GET['openid_signed']);
 
-    		foreach ($signed as $item) {
-    			$val = $_GET['openid_' . str_replace('.', '_', $item)];
-    			$params['openid.' . $item] = get_magic_quotes_gpc() ? stripslashes($val) : $val; 
-    		}
+            foreach ($signed as $item) {
+                $val = $_GET['openid_' . str_replace('.', '_', $item)];
+                $params['openid.' . $item] = get_magic_quotes_gpc() ? stripslashes($val) : $val; 
+            }
 
-    		$params['openid.mode'] = 'check_authentication';
+            $params['openid.mode'] = 'check_authentication';
 
-    		$data =  http_build_query($params);
+            $data =  http_build_query($params);
 
-    		$context = stream_context_create(array(
-    			'http' => array(
-    				'method' => 'POST',
-    				'header' => 
-    				"Accept-language: en\r\n".
-    				"Content-type: application/x-www-form-urlencoded\r\n" .
-    				"Content-Length: " . strlen($data) . "\r\n",
-    				'content' => $data,
-    				),
-			));
+            $context = stream_context_create(array(
+                'http' => array(
+                    'method' => 'POST',
+                    'header' => 
+                    "Accept-language: en\r\n".
+                    "Content-type: application/x-www-form-urlencoded\r\n" .
+                    "Content-Length: " . strlen($data) . "\r\n",
+                    'content' => $data,
+                    ),
+            ));
 
-    		$result = file_get_contents(self::$openId, false, $context);
+            $result = file_get_contents(self::$openId, false, $context);
 
-    		preg_match("#^http://steamcommunity.com/openid/id/([0-9]{17,25})#", $_GET['openid_claimed_id'], $matches);
+            preg_match("#^http://steamcommunity.com/openid/id/([0-9]{17,25})#", $_GET['openid_claimed_id'], $matches);
 
-    		$steamID64 = is_numeric($matches[1]) ? $matches[1] : 0;
+            $steamID64 = is_numeric($matches[1]) ? $matches[1] : 0;
 
-    		$response = preg_match("#is_valid\s*:\s*true#i", $result) == 1 ? $steamID64 : null;
+            $response = preg_match("#is_valid\s*:\s*true#i", $result) == 1 ? $steamID64 : null;
 
-    	} catch (Exception $e) {
-    		$response = null;
-    	}
+        } catch (Exception $e) {
+            $response = null;
+        }
 
-    	if (is_null($response)) {
-    		throw new Exception('The Steam login request timed out or was invalid');
-    	}
+        if (is_null($response)) {
+            throw new Exception('The Steam login request timed out or was invalid');
+        }
 
-    	return $response;
+        return $response;
     }
 }
