c1dea90ffcc1f3f84688e6699f2d180f5bcb820c
Arnaud Ligny <arnaud@ligny.org>
2014-10-27 23:48:45 +0100

    Update SteamLogin.php

--- src/Ehesp/SteamLogin/Laravel/Facades/SteamLogin.php
+++ src/Ehesp/SteamLogin/Laravel/Facades/SteamLogin.php
@@ -5,12 +5,12 @@ use Illuminate\Support\Facades\Facade;
 
 class SteamLogin extends Facade
 {
-	/**
-	 * Get the registered name of the component.
-	 *
-	 * @return string
-	 */
-	protected static function getFacadeAccessor() {
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() {
         return 'steamlogin';
-	}
+    }
 }
